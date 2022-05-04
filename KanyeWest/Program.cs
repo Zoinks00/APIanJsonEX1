da058018e37df79bf82294ea445db3fa200d89b7
Forrest Rabideau <forrest.rabideau@gmail.com>
2022-05-04 00:02:02 -0700

    initial commit

---
+++ KanyeWest/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Net.Http;
+using Newtonsoft.Json.Linq;
+
+namespace KanyeWest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //for statement that counts to 10 
+            //new  randomly picked kenya quotes for every even number
+            //new randomly picked ron quotes for every odd number.
+            for(int i =0; i <10; i++)
+            {
+                if(i %2 ==0 )
+                {
+                    RandomQuotes.KQuote();
+                }
+                else //if (i % 2 != 0)
+                {
+                    RandomQuotes.RQuote();
+                }
+            }//end for
+        }//end main
+    }
+}
---
+++ KanyeWest/RandomQuotes.cs
@@ -0,0 +1,41 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KanyeWest
+{
+   public class RandomQuotes
+    {
+        public static void KQuote()
+        {
+            //create new instance of client
+            var client = new HttpClient();
+            //ref kanye URL
+            var kanyeURL = "https://api.kanye.rest";
+            // get request to return url from kanyeURL
+            var kanyeResponse = client.GetStringAsync(kanyeURL).Result;
+            //move through URL to find quotes
+            var kanyeQuote = JObject.Parse(kanyeResponse).GetValue("quote").ToString();
+
+            Console.WriteLine($"Kanye says {kanyeQuote}");
+        }
+
+        public static void RQuote()
+        {
+            var client = new HttpClient();
+            //ref ron swanson URL
+            var swansonURL = "https://ron-swanson-quotes.herokuapp.com/v2/quotes";
+            // get request to return url from swansonURL
+            var swansonResponse = client.GetStringAsync(swansonURL).Result;
+            //move through URL to find quotes
+            var ronQuote = JArray.Parse(swansonResponse).ToString().Replace('[', ' ').Replace(']', ' ').Trim();
+
+            Console.WriteLine($"Ron says {ronQuote}\n");
+        }
+
+    }
+}
